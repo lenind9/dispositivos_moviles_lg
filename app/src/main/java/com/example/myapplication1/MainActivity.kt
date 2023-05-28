a742e7f9cd0177397b37c828ecf183da68eef294
Lenin Guananga <lenindgc@hotmail.com>
2023-05-28 11:02:00 -0500

    Segunda clase

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.myapplication1
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var boton3 = findViewById<Button>(R.id.button3)
+        var txtBuscar = findViewById<TextView>(R.id.txt_buscar)
+
+        boton3.text = "INGRESAR"
+        boton3.setOnClickListener {
+            txtBuscar.text = "El evento se ha ejecutado"
+       //     Toast.makeText(this, "Este es un ejemplo", Toast.LENGTH_SHORT)
+        //        .show()
+
+            var f = Snackbar.make(boton3, "Este es otro mensaje", Snackbar.LENGTH_SHORT)
+            f.setBackgroundTint(R.color.white).show()
+
+        }
     }
 }
